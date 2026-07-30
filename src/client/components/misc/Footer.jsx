9221a958ca9d4b6bb6fed3de0f03dbbba14656fc
Mohammed Razin <153722372+Mohammed-razin-cr@users.noreply.github.com>
2026-07-31 00:03:17 +0530

    commit62

--- src/client/components/misc/Footer.jsx
+++ src/client/components/misc/Footer.jsx
@@ -146,7 +146,7 @@ const Footer = (props) => {
       <FooterInner>
         <FooterLeft>
           <LogoWrapper>
-            <svg viewBox="0 0 24 24" fill="none">
+            <svg viewBox="0 0 24 24" fill="none" aria-hidden="true">
               <path
                 d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2z"
                 fill={colors.primary}
